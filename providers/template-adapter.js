11a8c129acc329fca6d245a4619ca19f101ebe6a
star884 <136379187+star884@users.noreply.github.com>
2026-09-04 23:29:17 +0530

    Update template-adapter.js

--- providers/template-adapter.js
+++ providers/template-adapter.js
@@ -2,28 +2,21 @@
 export const name = 'TemplateProvider';
 
 /**
- * create() must return an object implementing:
+ * create() returns provider instance that implements:
  * - async search(query) -> { results: [ { id, title, ... } ] }
- * - async fetchAnimeInfo(animeId) -> { id, title: { english, romaji }, episodes: [ { id, number, title }, ... ] }
- * - async fetchEpisodeSources(episodeId) -> { sources: [ { url, quality, isM3U8, server }, ... ] }
- *
- * Implementations can use axios/fetch + cheerio to scrape HTML, or call provider APIs.
+ * - async fetchAnimeInfo(animeId) -> { id, title: { english, romaji }, episodes: [ { id, number, title } ] }
+ * - async fetchEpisodeSources(episodeId) -> { sources: [ { url, quality, isM3U8, server } ] }
  */
 export function create() {
   return {
     async search(q) {
-      // TODO: implement actual search
-      // Example shape:
+      // implement with axios/cheerio or an API
       return { results: [] };
     },
-
     async fetchAnimeInfo(animeId) {
-      // TODO: implement actual fetch
       return { id: animeId, title: { english: null, romaji: animeId }, episodes: [] };
     },
-
     async fetchEpisodeSources(episodeId) {
-      // TODO: implement actual fetch
       return { sources: [] };
     }
   };
