24d7a02fe500466f8981e91e83058153bd34a509
star884 <136379187+star884@users.noreply.github.com>
2026-09-04 23:28:49 +0530

    Update nineanime.js

--- providers/nineanime.js
+++ providers/nineanime.js
@@ -4,8 +4,7 @@ export const name = 'NineAnimeAdapter';
 
 export function create() {
   const ANIME = Consumet.ANIME ?? Consumet.default?.ANIME ?? null;
-
-  const possibleNames = ['NineAnime', 'Nineanime', '9anime', 'Nine']; // try common variants
+  const possibleNames = ['NineAnime', 'Nineanime', 'Nine', 'AnimeNine', 'Nine9'];
   for (const n of possibleNames) {
     if (ANIME && typeof ANIME[n] === 'function') {
       const inst = new ANIME[n]();
@@ -18,14 +17,8 @@ export function create() {
   }
 
   return {
-    async search(q) {
-      throw new Error('9anime adapter not implemented locally. Please port the CloudStream 9anime extension into providers/nineanime.js (see providers/template-adapter.js).');
-    },
-    async fetchAnimeInfo(id) {
-      throw new Error('9anime adapter not implemented locally. Please port the CloudStream 9anime extension into providers/nineanime.js.');
-    },
-    async fetchEpisodeSources(id) {
-      throw new Error('9anime adapter not implemented locally. Please port the CloudStream 9anime extension into providers/nineanime.js.');
-    }
+    async search() { throw new Error('NineAnimeAdapter: not implemented locally.'); },
+    async fetchAnimeInfo() { throw new Error('NineAnimeAdapter: not implemented locally.'); },
+    async fetchEpisodeSources() { throw new Error('NineAnimeAdapter: not implemented locally.'); }
   };
 }
